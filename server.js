ac6dde032b8fa9d80942dc1fc2217dbfba690095
gilmargs <gilmar.silva@ccc.ufcg.edu.br>
2024-11-20 19:34:29 -0300

    recuperando objetos guardados em um array

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
 import express from "express";
 
+const posts = [
+    {
+        id: 1,
+        descricao: "Foto teste",
+        imagem: "https://placecats.com/millie/300/150"
+    },
+    {
+        id: 2,
+        descricao: "Paisagem incrível",
+        imagem: "https://source.unsplash.com/random/300x200"
+    },
+];
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Servidor escutando ...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Hello, World!");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+function bucarPostPorId(id){
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+};
+
+app.get("/posts/:id", (req, res) => {
+    const index = bucarPostPorId(req.params.id);
+    res.status(200).json(posts[index]);
 });
