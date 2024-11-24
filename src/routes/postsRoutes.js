b00ff81f3cd9c44e7d16dab210ee0ee2454b82d6
gilmargs <gilmar.silva@ccc.ufcg.edu.br>
2024-11-24 14:25:18 -0300

    implementando armazenamento de Upload e Imagens

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsController.js";
+import multer from "multer";
+import { listarPosts, salvarNovoPost, uploadImagem } from "../controllers/postsController.js";
+
+const upload = multer({dest: "./uploads"});
 
 const routes = (app) => {
     app.use(express.json());
     app.get("/posts", listarPosts);
+    app.post("/posts", salvarNovoPost);
+    app.post("/upload", upload.single("imagem"), uploadImagem);
 };
 
 export default routes;
